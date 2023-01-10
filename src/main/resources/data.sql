d2c9c4953f593e9f3b286488691014c7d0da5a10
AKPatrashkov18 <AKPatrashkov18@codingburgas.bg>
2023-01-10 15:36:22 +0200

    Commitings

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO Player (ID, Name, Nationality, Birth_date, Titles)
+VALUES (1,'jimmi','jimmi','1987-05-22', 81);
+
+INSERT INTO Player (ID, Name, Nationality, Birth_date, Titles)
+VALUES (2,'jimmichu','jimmi','1988-05-22', 10);
+
+INSERT INTO Player (ID, Name, Nationality, Birth_date, Titles)
+VALUES (3,'jimmichu lui','jimmi','1989-05-22', 15);
